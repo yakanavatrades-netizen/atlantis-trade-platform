af9f0331a442e130f6915f38f57fa8ffb6e94f29
yakanavatrades-netizen <yakanavatrades@gmail.com>
2025-11-24 22:34:12 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
 
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "member" }, // member | admin
+    name: { type: String },
+    membershipActive: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
