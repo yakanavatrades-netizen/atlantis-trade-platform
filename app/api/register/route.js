cbe4135ace2a58d1b8a6068b7e1b815dda89ece4
yakanavatrades-netizen <yakanavatrades@gmail.com>
2025-11-24 22:47:40 +0100

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,1 +1,32 @@
 
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+    const { email, password, name } = await req.json();
+
+    if (!email || !password)
+      return new Response("Missing data", { status: 400 });
+
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return new Response("Email already registered", { status: 409 });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    await User.create({
+      email,
+      password: hashed,
+      name,
+      role: "member",
+      membershipActive: false,
+    });
+
+    return new Response("User created", { status: 201 });
+  } catch (error) {
+    return new Response("Server error", { status: 500 });
+  }
+}
