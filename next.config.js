38ed01c0844bde8c52650b7a63ebd17e9afc812a
yakanavatrades-netizen <yakanavatrades@gmail.com>
2025-11-25 18:51:36 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["/"] = ".";
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": __dirname,
+    };
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
-
